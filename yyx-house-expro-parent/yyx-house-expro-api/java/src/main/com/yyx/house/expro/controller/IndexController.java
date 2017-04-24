10a3c8304b57ecd9eedb11df8b20778a3867f3c0
zhangya <114742054@qq.com>
2017-04-24 17:49:59 +0800

    修改配置文件

--- yyx-house-expro-parent/yyx-house-expro-api/java/src/main/com/yyx/house/expro/controller/IndexController.java
+++
@@ -1,7 +0,0 @@
-package com.yyx.house.expro.controller;
-
-/**
- * Created by zhangya on 2017/4/24.
- */
-public class IndexController {
-}
---
+++ yyx-house-expro-parent/yyx-house-expro-api/java/src/main/com/yyx/house/expro/web/IndexController.java
@@ -0,0 +1,10 @@
+package com.yyx.house.expro.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhangya on 2017/4/24.
+ */
+@RestController
+public class IndexController {
+}
