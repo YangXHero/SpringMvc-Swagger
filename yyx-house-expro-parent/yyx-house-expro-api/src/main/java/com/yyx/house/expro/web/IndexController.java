57ab574bda5dcc15bef5f71b2132c032c7594d34
zhangya <114742054@qq.com>
2017-04-25 11:16:09 +0800

    修改配置文件

--- yyx-house-expro-parent/yyx-house-expro-api/src/main/java/com/yyx/house/expro/web/IndexController.java
+++ yyx-house-expro-parent/yyx-house-expro-api/src/main/java/com/yyx/house/expro/web/IndexController.java
@@ -15,7 +15,7 @@ public class IndexController {
 
     @RequestMapping(value="/login", method = RequestMethod.GET)
     @ApiOperation(value = "用户登录", httpMethod = "GET", response = ResponseEntity.class, notes = "用户登录")
-    public ResponseEntity login(@ApiParam(required = true, name = "userName", value = "用户名") @PathVariable String name) throws Exception{
+    public ResponseEntity login(@ApiParam(required = true, name = "userName", value = "用户名")  String userName,String password) throws Exception{
 //        UcUser ucUser = ucUserManager.getUserByName(name);
 
 //        if(ucUser != null) {
