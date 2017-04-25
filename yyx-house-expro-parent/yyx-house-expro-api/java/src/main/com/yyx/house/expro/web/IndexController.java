35949d2a4d2b3d346ed39c3a47c4cabde085f244
zhangya <114742054@qq.com>
2017-04-25 10:05:36 +0800

    修改配置文件

--- yyx-house-expro-parent/yyx-house-expro-api/java/src/main/com/yyx/house/expro/web/IndexController.java
+++ yyx-house-expro-parent/yyx-house-expro-api/java/src/main/com/yyx/house/expro/web/IndexController.java
@@ -1,11 +1,32 @@
 package com.yyx.house.expro.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.yyx.core.entity.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by zhangya on 2017/4/24.
  */
 @RestController
-
 public class IndexController {
+
+
+
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    @ApiOperation(value = "用户登录", httpMethod = "GET", response = ResponseEntity.class, notes = "用户登录")
+    public ResponseEntity login(@ApiParam(required = true, name = "userName", value = "用户名") @PathVariable String name) throws Exception{
+//        UcUser ucUser = ucUserManager.getUserByName(name);
+
+//        if(ucUser != null) {
+//            ApiResult<UcUser> result = new ApiResult<UcUser>();
+//            result.setCode(ResultCode.SUCCESS.getCode());
+//            result.setData(ucUser);
+//            return result;
+//        } else {
+//            throw new BusinessException("根据{name=" + name + "}获取不到UcUser对象");
+//        }
+        ResponseEntity testEntity = new ResponseEntity(new Integer(1),"用户登录成功");
+        return testEntity;
+    }
 }
