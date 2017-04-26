bb78201af3ec7fc64e8a64a5566a161c4065882a
zhangya <114742054@qq.com>
2017-04-26 13:17:02 +0800

    修改配置文件

---
+++ yyx-house-expro-parent/yyx-house-expro-api/src/main/java/com/yyx/config/SwaggerUIConfig.java
@@ -0,0 +1,26 @@
+package com.yyx.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by zhangya on 2017/4/26.
+ */
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = { "com.yyx.house.expro.web" })
+public class SwaggerUIConfig  extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
--- yyx-house-expro-parent/yyx-house-expro-api/src/main/java/com/yyx/house/expro/web/IndexController.java
+++ yyx-house-expro-parent/yyx-house-expro-api/src/main/java/com/yyx/house/expro/web/IndexController.java
@@ -2,6 +2,7 @@ package com.yyx.house.expro.web;
 
 
 import com.yyx.core.entity.ResponseEntity;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
@@ -13,17 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by zhangya on 2017/4/24.
  */
+@Api(value = "contacts-api", description = "用户登录", position = 5)
 @RestController
 public class IndexController {
 
 
 
-    @RequestMapping(value="/login", method = RequestMethod.GET)
+
     @ApiOperation(value = "用户登录", httpMethod = "GET", response = ResponseEntity.class, notes = "用户登录")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "userName", value = "用户名", required = true, dataType = "String"),
             @ApiImplicitParam(name = "password", value = "密码", required = true, dataType = "String")
     })
+    @RequestMapping(value="/login", method = RequestMethod.GET)
     public ResponseEntity login(String userName, String password) throws Exception{
 //        UcUser ucUser = ucUserManager.getUserByName(name);
 
